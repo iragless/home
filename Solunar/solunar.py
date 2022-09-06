dc56758466a132469435c47a64785844acb195b9
iragless <ian@webvisions.com.au>
2022-09-06 01:07:06 +0000

    testing sunrise api

--- Solunar/solunar.py
+++ Solunar/solunar.py
@@ -1,7 +1,8 @@
 import json
 from urllib.request import urlopen
 
-with urlopen("https://api.solunar.org/solunar/-34.99,138.52,20220907,10") as response:
+# solunar api with urlopen("https://api.solunar.org/solunar/-34.99,138.52,20220907,10") as response:
+with urlopen("https://api.sunrise-sunset.org/json?lat=-34.99&lng=138.52&date=today") as response: #sunrise api
     source = response.read()
 
 data = json.loads(source)
